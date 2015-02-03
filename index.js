b19b976362f7174354a0a47d753b8828d8cd4124
Brenton Morris & Dan Langevin <brenton+dan@vericred.com>
2015-02-03 18:22:31 -0500

    First pass at integration with broccoli-pizzazz

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 /* jshint node: true */
 'use strict';
 
+var BroccoliPizzazz = require('broccoli-pizzazz');
+
 module.exports = {
-  name: 'ember-cli-pizzazz'
+  name: 'ember-cli-pizzazz',
+  included: function (app) {
+    this.app = app;
+  },
+
+  liveReloadPath: function () {
+    return '/ember-cli-live-reload.js';
+  },
+
+  treeForPublic: function (type) {
+    return new BroccoliPizzazz(
+      this.app.trees.styles,{
+        applicationCssPath: this.app.options.outputPaths.app.css.app,
+        liveReloadPath: this.liveReloadPath()
+      }
+    );
+  }
 };
